bb51268e6546ebe2ac389c8c3fd18ab5791a5bbd
your name <your@email.com>
2023-07-20 17:14:04 -0300

    Finish Form Page

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+/* eslint-disable */
 
-const allProperties = () => {
+import React from 'react'
+import { Add } from '@mui/icons-material'
+import { useList } from "@pankod/refine-core"
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import { useNavigate } from "react-router-dom";
+
+import { PropertyCard, CustomButton } from 'components';
+
+
+
+const AllProperties = () => {
+
+  const navigation = useNavigate();
+
   return (
-    <div>all-properties</div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#1114sd">Todas las propiedades</Typography>
+        <CustomButton title="Añadir Propiedad" handleClick={() => navigation("/properties/create")} backgroundColor="#475be8" color="#fcfcfc" icon={<Add />}/>
+      </Stack>
+    </Box>
   )
 }
 
-export default allProperties
+export default AllProperties
--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+/* eslint-disable */
 
-const createProperty = () => {
+import React from "react";
+import { useState } from "react";
+import { useForm } from "@refinedev/react-hook-form";
+import { useGetIdentity } from "@refinedev/core";
+import { FieldValues } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+import Form from "components/common/Form";
+
+const CreateProperty = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity({
+    v3LegacyAuthProviderCompatible: true,
+  });
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+      new Promise<string>((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result as string);
+        fileReader.readAsDataURL(readFile);
+      });
+
+    reader(file).then((result: string) =>
+      setPropertyImage({ name: file?.name, url: result })
+    );
+  };
+
+  const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) return alert("Please select an image");
+
+    await onFinish({
+      ...data,
+      photo: propertyImage.url,
+      email: user.email,
+    });
+  };
+
   return (
-    <div>create-property</div>
-  )
-}
+    <Form
+      type="Create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
+  );
+};
 
-export default createProperty
+export default CreateProperty;
